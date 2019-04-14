f8e4f42a2561ba8a360f1c08c9f9402bf0e1cd26
Rishabh-1999 <rishabhanand33@gmail.com>
2019-04-14 18:31:59 +0530

    Add

--- app/src/main/java/android/example/autoattendie/SignUp.java
+++ app/src/main/java/android/example/autoattendie/SignUp.java
@@ -5,13 +5,49 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignUp extends Activity {
+    Button singUp;
+    EditText Id;
+    EditText password1;
+    EditText password2;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.signup);
+        singUp=(Button) findViewById(R.id.signup);
+        Id=(EditText)findViewById(R.id.enter_id);
+        password1=(EditText)findViewById(R.id.enter_password1);
+        password2=(EditText)findViewById(R.id.enter_password2);
+        singUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(!Id.getText().toString().isEmpty() && !password1.getText().toString().isEmpty() &&
+                        !password2.getText().toString().isEmpty())
+                {
+                    String p1=password1.getText().toString().trim();
+                    String p2=password2.getText().toString().trim();
+                    if(p1.equals(p2))
+                    {
+                        LoginDB db= new LoginDB(SignUp.this);
+                        db.open();
+                            db.createEntry(Id.getText().toString().trim(), password1.getText().toString());
+                        Toast.makeText(SignUp.this, "Account created", Toast.LENGTH_SHORT).show();
+                        db.close();
+                        login(v);
+                    }
+                    else
+                        Toast.makeText(SignUp.this, "Password Does not match", Toast.LENGTH_SHORT).show();
+                }
+                else
+                    Toast.makeText(SignUp.this, "Check your Details", Toast.LENGTH_SHORT).show();
 
+            }
+        });
     }
     public void login(View view) {
         Intent login = new Intent(this, Login.class);
