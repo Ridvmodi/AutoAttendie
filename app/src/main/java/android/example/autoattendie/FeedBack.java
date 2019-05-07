8199ab7fe8ed736e125f8b43758f881290075206
rajat-29 <rajat295gupta@gmail.com>
2019-05-07 16:40:45 +0530

    intent

--- app/src/main/java/android/example/autoattendie/FeedBack.java
+++ app/src/main/java/android/example/autoattendie/FeedBack.java
@@ -1,8 +1,14 @@
 package android.example.autoattendie;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.RadioButton;
 
 public class FeedBack extends AppCompatActivity {
 
@@ -13,5 +19,43 @@ public class FeedBack extends AppCompatActivity {
         android.support.v7.widget.Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         toolbar.setTitle("FeedBack");
         setSupportActionBar(toolbar);
+    }
+
+    public void submit_req(View view) {
+
+
+        EditText cust_msg = (EditText) findViewById(R.id.message);
+        String value = cust_msg.getText().toString();
+
+        RadioButton bugcheck = (RadioButton) findViewById(R.id.bug_id);
+        boolean bugss = bugcheck.isChecked();
+
+        RadioButton suggest = (RadioButton) findViewById(R.id.suggestion_id);
+        boolean suggs = suggest.isChecked();
+
+        String subj = checkv(bugss, suggs);
+
+        Intent intent = new Intent(Intent.ACTION_SENDTO);
+        intent.setData(Uri.parse("mailto:"));
+        intent.putExtra(Intent.EXTRA_SUBJECT,subj);
+        intent.putExtra(Intent.EXTRA_TEXT, value);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        }
+
+    }
+
+    public String checkv(boolean bugss,boolean suggs) {
+        String m = "Hello, There is a ";
+        if(bugss) {
+            m =  m + "Bug";
+        }
+        else if(suggs) {
+            m =  m + "Suggestion";
+        }
+        if(bugss && suggs) {
+            m = m + "Bug and Suggestion";
+        }
+        return m;
     }
 }
